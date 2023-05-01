e80cbc6534c498db27ff42edbd54cf5ad00e081d
Steve Pflug <stephen@pflug.us>
2023-05-01 14:21:43 -0500

    first commit

---
+++ src/main/java/com/pfinance/pfinancefullstack/PfinanceFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.pfinance.pfinancefullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfinanceFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfinanceFullstackApplication.class, args);
+    }
+
+}
