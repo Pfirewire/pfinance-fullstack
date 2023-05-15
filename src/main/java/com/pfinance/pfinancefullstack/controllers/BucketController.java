67d88cbd55f33d7080f75d4a0574449e8788b583
Stephen Pflug <stephen@pflug.us>
2023-05-14 22:10:55 -0500

    creating Bucket rest controller

---
+++ src/main/java/com/pfinance/pfinancefullstack/controllers/BucketController.java
@@ -0,0 +1,13 @@
+package com.pfinance.pfinancefullstack.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class BucketController {
+
+}
