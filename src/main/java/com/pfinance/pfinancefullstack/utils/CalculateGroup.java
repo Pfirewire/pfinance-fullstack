fc72d9775a13f6b2a16ea64c6504dd3f7724a421
Steve Pflug <stephen@pflug.us>
2023-05-24 11:57:08 -0500

    renamed method to calculate all amounts in group. added method to add one bucket amount

--- src/main/java/com/pfinance/pfinancefullstack/utils/CalculateGroup.java
+++ src/main/java/com/pfinance/pfinancefullstack/utils/CalculateGroup.java
@@ -5,7 +5,7 @@ import com.pfinance.pfinancefullstack.models.Group;
 import com.pfinance.pfinancefullstack.repositories.GroupRepository;
 
 public class CalculateGroup {
-    public static void amounts(Group group, GroupRepository groupDao) {
+    public static void allAmounts(Group group, GroupRepository groupDao) {
         double current = 0;
         double recurring = 0;
         double maximum = 0;
@@ -17,6 +17,13 @@ public class CalculateGroup {
         group.setTotalCurrentAmount(current);
         group.setTotalRecurringAmount(recurring);
         group.setTotalMaximumAmount(maximum);
+        groupDao.save(group);
+    }
+
+    public static void addAmount(Group group, Bucket bucket, GroupRepository groupDao) {
+        group.setTotalCurrentAmount(group.getTotalCurrentAmount() + bucket.getCurrentAmount());
+        group.setTotalRecurringAmount(group.getTotalRecurringAmount() + bucket.getRecurringAmount());
+        group.setTotalMaximumAmount(group.getTotalMaximumAmount() + bucket.getMaximumAmount());
         groupDao.save(group);
     }
 }
