f8ed56bfe18fac19e1297fcdc5818de525e2a02f
Steve Pflug <stephen@pflug.us>
2023-05-25 08:29:19 -0500

    added methods to UserController to register new user

--- src/main/java/com/pfinance/pfinancefullstack/controllers/UserController.java
+++ src/main/java/com/pfinance/pfinancefullstack/controllers/UserController.java
@@ -3,13 +3,15 @@ package com.pfinance.pfinancefullstack.controllers;
 import com.pfinance.pfinancefullstack.models.User;
 import com.pfinance.pfinancefullstack.models.UserStatus;
 import com.pfinance.pfinancefullstack.repositories.UserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,9 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userDao) {
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user, HttpServletRequest httpServletRequest) {
+        String plainPassword = user.getPassword();
+        // Hashing password
+        String hash = passwordEncoder.encode(user.getPassword());
+        // Setting user password to the hash and saving user to table
+        user.setPassword(hash);
+        userDao.save(user);
+        if(authWithHttpServletRequest(httpServletRequest, user.getUsername(), plainPassword)) {
+            return new ResponseEntity<>("User signed-in successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("User registration unsuccessful.", HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/status")
@@ -34,5 +53,15 @@ public class UserController {
             if(user.getAccessToken() != null) tokenExists = true;
         }
         return new UserStatus(userExists, tokenExists);
+    }
+
+    private boolean authWithHttpServletRequest(HttpServletRequest request, String username, String password) {
+        try {
+            request.login(username, password);
+            return true;
+        } catch (ServletException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
